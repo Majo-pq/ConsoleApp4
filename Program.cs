3874afdd9cf058c05568f402a455a4fe66e70ee3
Maria Jose Pecina Quintana <h21050059@monclova.tecnm.mx>
2021-10-15 10:28:45 -0500

    Se agrero el main del Punto3D

--- Program.cs
+++ Program.cs
@@ -20,9 +20,11 @@ namespace ConsoleApp4
 
             Console.WriteLine(b.ToString());
 
+            Punto3D z;
+            z = new Punto3D();
+            Console.WriteLine(z.ToString());
             
 
-           
             Punto3D e;
             e = new Punto3D(3, 4, 5.1);
             Console.WriteLine(e.ToString());
