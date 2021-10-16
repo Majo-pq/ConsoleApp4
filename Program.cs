3f184d17903b0a480f9a2a05feca18301c580856
Maria Jose Pecina Quintana <h21050059@monclova.tecnm.mx>
2021-10-16 00:00:49 -0500

    Se agrego el main del Punto3D

--- Program.cs
+++ Program.cs
@@ -19,18 +19,13 @@ namespace ConsoleApp4
             b = new Punto2D(2,2);
 
             Console.WriteLine(b.ToString());
-
-            //Punto3D z;
-            //z = new Punto3D();
-            //Console.WriteLine(z.ToString());
             
-
             Punto3D e;
-            e = new Punto3D(3, 4, 5.1);
+            e = new Punto3D(3, 8, 5.1);
             Console.WriteLine(e.ToString());
 
             Punto3D f;
-            f = new Punto3D(6, 7, 4);
+            f = new Punto3D(6, 7, 4.2);
             Console.WriteLine(f.ToString());
 
             double d = e.Distancia(f);
